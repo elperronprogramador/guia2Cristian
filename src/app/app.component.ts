8d4386b36463141eb5ae013bc700b83ec037cdc0
christian <cristianyamith@hotmail.com>
2024-09-13 23:58:42 -0500

    feat: creacion de html y styles.css, poniendo en practica las etiquetas y estilos solicitados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,22 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'guia2Cristian';
+  titulo = 'PERRONA';
+  avatar= 'https://images.vexels.com/media/users/3/227565/isolated/preview/f77e3012e7b4ee1436dd818c420acafe-logotipo-frontal-de-shephard-alem-n.png';
+  video = '';
+  link1 = 'https://www.youtube.com/watch?v=itVGgPBxkQo' ;
+  link2 = 'https://www.suzuki.com.co/motocicletas';
+  link3 = 'https://colorhunt.co/';
   
-  avatar= 'https://www.w3schools.com/howto/img_avatar.png'
+  inventario ={
+    nombrep1:'Gato',
+    precio1: 100.000,
+    cantidad1: 100,
+    nombrep2:'Perro',
+    precio2: 80.000,
+    cantidad2: 20,
+    nombrep3:'Ratones',
+    precio3: 200.000,
+    cantidad3: 50,
+  }
 }
